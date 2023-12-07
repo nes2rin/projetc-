a13f7fe0863c905dfef26db0b2b013515337c068
eyahamdi <131485525+eyahamdi@users.noreply.github.com>
2023-12-07 05:12:03 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include "login.h"
 #include <QApplication>
 #include <QMessageBox>
@@ -32,23 +31,3 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
-=======
-#include "mainwindow.h"
-#include <QApplication>
-#include "connexion.h"
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    connection c;
-    bool cnx = c.createconnection();
-    if(cnx)
-        w.show();
-    else
-        QMessageBox::critical(nullptr,QObject::tr("ERREUR!"),
-                              QObject::tr("Data Base connection failed. \n"
-                                          "Click me to exit."), QMessageBox::Cancel);
-    return a.exec();
-}
- 
--- materiel.h
+++ materiel.h
@@ -1,4 +1,3 @@
-
 #ifndef MATERIEL_H
 #define MATERIEL_H
 #include <QString>
@@ -27,33 +26,3 @@ public:
 };
 
 #endif // MATERIEL_H
-=======
-#ifndef MATERIEL_H
-#define MATERIEL_H
-#include <QString>
-#include <QSqlDatabase>
-#include <QSqlQuery>
-#include <QSqlQueryModel>
-#include "smtp.h"
-
-class materiel
-{
-private:
-    int id,age;
-    QString matricul,nom_machine,etat;
-    float capacite,prix;
-public:
-    materiel();
-    materiel(int,int,QString,QString,QString,float,float);
-    //CRUD functions
-    bool ajouterMateriel();
-    QSqlQueryModel * afficherMateriel();
-    bool supprimerMateriel(int);
-    bool modifierMateriel();
-    QSqlQueryModel * rechercherMateriel(QString recherche);
-    QSqlQueryModel * triMateriel(QString tri);
-    QVector<int> statMateriel();
-};
-
-#endif // MATERIEL_H
-
